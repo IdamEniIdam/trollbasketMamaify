fb2e12a6bdd129c1937d82aa6594373248438c86
IdamEniIdam <idameni89@gmail.com>
2021-06-18 10:56:06 -0400

    Backend integration still working some featires

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ mongoose.connect(
     useCreateIndex: true,
   },
   () => {
-    const port = process.env.PORT || 3000;
+    const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
     // app.listen(process.env.PORT, ip);
