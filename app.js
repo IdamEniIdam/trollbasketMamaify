3b6bb711c3d18cd140b2721d380b950a875e85d6
IdamEniIdam <idameni89@gmail.com>
2021-06-20 08:28:00 -0400

    Backend integration still working some featires

--- app.js
+++ app.js
@@ -26,11 +26,11 @@ const notification = require("./middlewares/pushNotification");
 //Connect to DB
 // const dbURI = process.env + "mongodb://localhost:27017/TrollBasket";
 
-const dbURI = "mongodb+srv://idameniidam:AlmightyGodid89@cluster0.g3vir.mongodb.net/test";
+const MONGO_URI = "mongodb+srv://idameniidam:AlmightyGodid89@cluster0.g3vir.mongodb.net/test";
 // const dbURI = DATABASEconnection;
 
 mongoose.connect(
-  dbURI,
+  MONGO_URI,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
