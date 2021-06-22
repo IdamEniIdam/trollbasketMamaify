93dfc5af889cd94d500fcb3ae1053dc848efaf1b
IdamEniIdam <idameni89@gmail.com>
2021-06-22 12:10:14 -0400

    Backend integration still working some featires

--- controllers/controller.product.js
+++ controllers/controller.product.js
@@ -33,8 +33,8 @@ const product_get = (req, res) => {
 
 const product_post = (req, res) => {
   // const host = process.env.HOST_NAME;
-  // const host = 'http://192.168.43.154:5000';
-  const host = 'https://mamaifytrollbasket.herokuapp.com/api';
+  const host = 'http://192.168.43.154:5000';
+  // const host = 'https://mamaifytrollbasket.herokuapp.com/api';
   const filename = req.body.filename.replace(/\+/g," ");
 
   if (!req.body || !req.file) {
@@ -90,8 +90,8 @@ const product_post = (req, res) => {
 const product_update = async (req, res) => {
   const id = req.params.id;
   // const host = process.env.HOST_NAME;
-  // const host = 'http://192.168.43.154:5000';
-  const host =  'https://mamaifytrollbasket.herokuapp.com/api';
+  const host = 'http://192.168.43.154:5000';
+  // const host =  'https://mamaifytrollbasket.herokuapp.com/api';
   let filename = "";
   let imageUrl = "";
   let resizeUrl = "";
